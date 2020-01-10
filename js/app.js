9d5c962a91e867b43540a81ec0b50d89543d35cb
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-10 12:42:03 +0000

    making progress on adding recipe items

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const newRecipeForm = document.querySelector('#')
+  const newRecipeForm = document.querySelector('#new-item-form')
+  newRecipeForm.addEventListener('submit', handleNewItemFormSubmit)
 })
+
+const handleNewItemFormSubmit = function (event) {
+  event.preventDefault()
+  const recipeListItem = createReadingListItem(event.target)
+  const recipeList = document.querySelector('#recipe-list')
+  recipeList.appendChild(recipeListItem)
+
+  event.target.reset()
+}
+
+const createNewRecipeListItem = function (form) {
+  const recipeListItem = document.createElement('li')
+  recipeListItem.classList.add('recipe-list-item')
+
+  const name = document.createElement('h2')
+  name.textContent = form.title.value
+  recipeListItem.appendChild(name)
+
+  return recipeListItem;
+
+}
