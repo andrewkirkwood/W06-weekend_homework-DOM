9990ca715e4e29678a3ac592aa5a821b0eeeca66
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-10 13:53:16 +0000

    got the app to add new recipes to the list

--- js/app.js
+++ js/app.js
@@ -5,20 +5,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const handleNewItemFormSubmit = function (event) {
   event.preventDefault()
-  const recipeListItem = createReadingListItem(event.target)
+  const recipeListItem = createRecipeListItem(event)
   const recipeList = document.querySelector('#recipe-list')
+  console.log(recipeList);
+  console.log(recipeListItem);
   recipeList.appendChild(recipeListItem)
 
   event.target.reset()
 }
 
-const createNewRecipeListItem = function (form) {
+const createRecipeListItem = function (form) {
   const recipeListItem = document.createElement('li')
   recipeListItem.classList.add('recipe-list-item')
 
   const name = document.createElement('h2')
-  name.textContent = form.title.value
+  name.textContent = form.target.name.value
   recipeListItem.appendChild(name)
+
+  const category = document.createElement('h3')
+  category.textContent = form.target.category.value
+  recipeListItem.appendChild(category)
+
+  const rating = document.createElement('h3')
+  rating.textContent = form.target.rating.value
+  recipeListItem.appendChild(rating)
 
   return recipeListItem;
 
